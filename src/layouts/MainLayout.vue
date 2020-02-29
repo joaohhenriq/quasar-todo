7605836de473728b3cb4352da0b444757328fb69
joaoh <joao_camargo10@hotmail.com>
2020-02-29 11:26:21 -0300

    drawer navigation and help page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -23,7 +23,7 @@
         style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd"
       >
         <q-list padding>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple exact to="/">
             <q-item-section avatar>
               <q-icon name="list" />
             </q-item-section>
@@ -31,7 +31,7 @@
             <q-item-section>Todo</q-item-section>
           </q-item>
 
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple exact to="/help">
             <q-item-section avatar>
               <q-icon name="help" />
             </q-item-section>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page>
+    <h5>todo</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
